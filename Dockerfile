c2eeb0cdc7993591cc8a11b36039489929393e85
Maxime Heckel <heckelmaxime@gmail.com>
2016-08-24 21:41:51 +0200

    add tools for react dev + update dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
-FROM alpine:latest
+FROM node:5-slim
 
 ADD . /
+RUN npm install --unsafe-perm=true --no-optional
 
 EXPOSE 8000
 
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+var webpack = require('webpack');
+
+module.exports = {
+  entry: [
+    'webpack-dev-server/client?http://localhost:8080',
+    'webpack/hot/only-dev-server',
+    './src/index.js'
+  ],
+  module: {
+    loaders: [{
+      test: /\.js?$/,
+      exclude: /node_modules/,
+      loader: 'babel',
+      query: {
+        presets: ['es2015', 'stage-0', 'react']
+      }
+    },
+    {
+      test: /\.css?$/,
+      include: /src/,
+      loaders: [
+        'style',
+        'css',
+        'postcss',
+        'autoprefixer?browsers=last 3 versions'
+      ]
+    }]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: './dist'
+  },
+  plugins: [
+      new webpack.NoErrorsPlugin()
+  ]
+};
