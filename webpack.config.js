c967fb51b4f9698899bb0a05c864f3f0e7469339
Maxime Heckel <heckelmaxime@gmail.com>
2016-08-25 18:44:54 +0200

    remove webpackdevserver from build

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,47 @@
+
 var webpack = require('webpack');
 
+var entryBase = ['./src/index.js'];
+
+if (process.env.ENV === 'DEV') {
+  entryBase.push('webpack-dev-server/client?http://localhost:8080');
+}
+
 module.exports = {
-  entry: [
-    'webpack-dev-server/client?http://localhost:8080',
-    './src/index.js'
-  ],
+  entry: entryBase,
   module: {
-    loaders: [{
-      test: /\.js?$/,
-      exclude: /node_modules/,
-      loader: 'babel',
-      query: {
-        presets: ['es2015', 'stage-0', 'react']
-      }
-    },
-    {
-      test: /\.css?$/,
-      include: /src/,
-      loaders: [
-        'style',
-        'css',
-        'postcss',
-        'autoprefixer?browsers=last 3 versions'
-      ]
-    }]
+    loaders: [
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'stage-0', 'react'],
+        },
+      },
+      {
+        test: /\.css?$/,
+        include: /src/,
+        loaders: [
+          'style',
+          'css',
+          'postcss',
+          'autoprefixer?browsers=last 3 versions',
+        ],
+      }],
   },
   resolve: {
-    extensions: ['', '.js', '.jsx']
+    extensions: ['', '.js', '.jsx'],
   },
   output: {
     path: __dirname + '/dist',
     publicPath: '/dist/',
-    filename: 'bundle.js'
+    filename: 'bundle.js',
   },
   devServer: {
-    contentBase: './'
+    contentBase: './',
   },
   plugins: [
-      new webpack.NoErrorsPlugin()
-  ]
+    new webpack.NoErrorsPlugin(),
+  ],
 };
