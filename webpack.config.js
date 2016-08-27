89bcc8d87303f462a61e4a39f907bdde2b3dc5cf
Maxime Heckel <heckelmaxime@gmail.com>
2016-08-27 15:37:55 +0200

    add postcss to project

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,8 @@ var webpack = require('webpack');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 var entryBase = ['./src/index.js'];
+var atImport = require('postcss-import');
+var vars = require('postcss-simple-vars');
 
 if (process.env.ENV === 'DEV') {
   entryBase.push('webpack-dev-server/client?http://localhost:8080');
@@ -21,15 +23,23 @@ module.exports = {
         },
       },
       {
-        test: /\.css/,
-        loaders: ['style', 'css'],
-        loader: ExtractTextPlugin.extract('css?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]'),
+        test: /\.css$/,
+        exclude: /(github-markdown|normalize|react-select)\.css$/,
+        // eslint-disable-next-line max-len
+        loader: ExtractTextPlugin.extract('style-loader', 'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss-loader', { allChunks: true }),
       },
     ],
   },
   resolve: {
     extensions: ['', '.js', '.jsx'],
   },
+  postcss: [
+    atImport({
+      path: ['node_modules', './src'],
+    }),
+    vars(),
+    // Other PostCSS plugins
+  ],
   output: {
     path: __dirname + '/dist',
     publicPath: '/dist/',
