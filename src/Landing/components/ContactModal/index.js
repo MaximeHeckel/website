9b21acbfcc92ce76100dd867937266864d89afa1
Maxime Heckel <heckelmaxime@gmail.com>
2016-10-01 21:37:09 -0700

    add contact form to contact modal

--- src/Landing/components/ContactModal/index.js
+++ src/Landing/components/ContactModal/index.js
@@ -3,10 +3,12 @@ import { connect } from 'react-redux';
 import {
   Modal,
   Title,
+  Button,
 } from '../../../components';
 import {
   closeModal,
 } from '../../actions/creators';
+import css from './styles.css';
 
 export class ContactModal extends Component {
   static propTypes = {
@@ -17,11 +19,28 @@ export class ContactModal extends Component {
 
   constructor(...args) {
     super(...args);
-    this.handleConfirm = ::this.handleConfirm;
+    this.handleSubmit = ::this.handleSubmit;
     this.handleCancel = ::this.handleCancel;
   }
 
-  handleConfirm() {
+  state = {
+    email: '',
+    subject: '',
+    text: '',
+  };
+
+  onChangeInput(field, e) {
+    if (field === 'email') {
+      this.setState({ email: e.target.value });
+    } else if (field === 'subject') {
+      this.setState({ subject: e.target.value });
+    } else {
+      this.setState({ text: e.target.value });
+    }
+    return;
+  }
+
+  handleSubmit() {
     console.log('SENDING EMAIL TO ' + this.props.email);
   }
 
@@ -33,6 +52,34 @@ export class ContactModal extends Component {
     return (
       <Modal isOpen onCloseCall={this.handleCancel}>
         <Title text={this.props.title} />
+        <div className={css.form}>
+          <input
+            className={css.input}
+            onChange={this.onChangeInput.bind(this, 'email')}
+            value={this.state.email}
+            placeholder="Email"
+          />
+          <input
+            className={css.input}
+            onChange={this.onChangeInput.bind(this, 'subject')}
+            value={this.state.subject}
+            placeholder="Subject"
+          />
+          <textarea
+            rows="10"
+            className={css.input}
+            onChange={this.onChangeInput.bind(this, 'text')}
+            placeholder="Message"
+          />
+        </div>
+        <div className={css.submit}>
+          <Button
+            onClick={() => this.handleSubmit()}
+            className={css.submitButton}
+          >
+            Submit
+          </Button>
+        </div>
       </Modal>
     );
   }
